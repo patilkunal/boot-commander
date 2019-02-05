28abf48c528af6d46e584fe5b482ced685724717
Kunal Patil <kunalpatil@yahoo.com>
2019-02-04 20:52:37 -0500

    Update DB Server script to accept external database file

--- db/hsqldb/dbserver.sh
+++ db/hsqldb/dbserver.sh
@@ -1,1 +1,24 @@
-java -cp hsqldb.jar org.hsqldb.Server -database.0 file:testcasedb.hsql -dbname.0 testcasedb 
+#!/bin/sh
+
+DATABASE=testcasedb.hsql
+DATAFILE=$DATABASE.script
+if [ $# -gt 0 ]
+then
+    DATABASE=$1/testcasedb.hsql
+    DATAFILE=$DATABASE.script
+    if [ -d $1  -a ! -f $DATAFILE ] 
+    then
+        cp baseschema.hsql.script $DATAFILE
+    else
+        echo "Directory $1 does not exist"
+    fi
+fi
+
+if [ -f $DATAFILE ]
+then
+    java -cp hsqldb.jar org.hsqldb.Server -database.0 file:$DATABASE -dbname.0 testcasedb 
+else
+    echo "Unable to find data file $DATAFILE"
+fi
+
+
