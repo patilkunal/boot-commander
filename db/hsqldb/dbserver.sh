7c7da6342d80d68344c15449c5a11ad9a8a62b30
Kunal Patil <kunalpatil@yahoo.com>
2019-03-20 14:39:42 -0400

    Docker file changes

--- db/hsqldb/dbserver.sh
+++ db/hsqldb/dbserver.sh
@@ -5,13 +5,19 @@ DATAFILE=$DATABASE.script
 
 if [ $# -gt 0 ]
 then
+    if [ ! -d $1 ]
+    then
+        echo "Directory $1 does not exist"
+        exit 1
+    fi
     DATABASE=$1/testcasedb.hsql
     DATAFILE=$DATABASE.script
-    if [ -d $1  -a ! -f $DATAFILE ] 
+    if [ ! -f $DATAFILE ] 
     then
+        echo "No Database setup. Copying base schema to $DATAFILE"
         cp baseschema.hsql.script $DATAFILE
     else
-        echo "Directory $1 does not exist"
+        echo "Database is already setup at $DATAFILE. Skipping copying base schema"
     fi
 fi
 
