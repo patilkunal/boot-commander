1c789b0a1d39a8ba45b168dde8e3131bc2582213
Kunal Patil <kunalpatil@yahoo.com>
2020-07-15 19:15:06 -0400

    Login and signup pages with route, validation

--- src/main/ionic-app/src/app/public/login/login.page.ts
+++ src/main/ionic-app/src/app/public/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -8,16 +10,36 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
-  userData = {
-    username: '',
-    password: ''
-  }
-  constructor(private router: Router) { }
+   loginForm: FormGroup;
+   isSubmitted = false;
+
+  constructor(private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required] ], 
+      password: ['', [Validators.required, Validators.minLength(6)] ] 
+    });
+    // phone: Validators.pattern('^[0-9]+$')
+    // email: Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
+  }
+
+  get errorControl() {
+    return this.loginForm.controls;
   }
 
   loginAction() {
-    this.router.navigate(['secured']);
+    this.isSubmitted = true;
+    if (!this.loginForm.valid) {
+      console.log('Please provide all the required values!')
+      return false;
+    } else {
+      console.log('Going to login using: ' + JSON.stringify(this.loginForm.value));
+      this.router.navigate(['secured']);
+    }
+  }
+
+  navigateTo(page) {
+    this.router.navigate([page]);
   }
 }
--- src/main/ionic-app/src/app/secured/secured.page.ts
+++ src/main/ionic-app/src/app/secured/secured.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-secured',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecuredPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  logoutAction() {
+    this.router.navigate(['/login']);
   }
 
 }
